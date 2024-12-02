28a04e35dbf29456a811e8578bee3640b973e1b0
Jeferson santos <jefersonrouue@gmail.com>
2024-12-02 17:59:17 -0300

    feat: added first part of flow could take the schedules and show it in chat

--- scenes/cancelarAgendamento/agendamentoToCancel.js
+++ scenes/cancelarAgendamento/agendamentoToCancel.js
@@ -3,6 +3,7 @@ import sequelize from "../../config/dataBase.js";
 import Usuario from "../../models/usuario.js";
 import Agendamento from "../../models/agendamento.js";
 import { Scenes } from "telegraf";
+import { where } from "sequelize";
 
 
 //instanciaar scene
@@ -11,7 +12,26 @@ const agendamentoToCancel = new Scenes.BaseScene('agendamentoToCancel');
 agendamentoToCancel.enter(async (ctx) => {
     try{
         //mensagem de entrada para cancelar agendamento
-        ctx.reply('você escolçheu cancelar agendamento')
+        ctx.reply('Você deseja cancelar algum dos seus agendamentos? 🤔📅\n\n' +
+             ' Por favor, informe o agendamento que deseja cancelar. ❌')
+        //buscar agendamentos
+        const chatIdAtual = ctx.chat.id //pegar chatid
+
+        //buscar agendamentos no db
+        const agendamentosLista = await Agendamento.findAll({where:{chatid_telegram: chatIdAtual}})
+        //verificar se existem agendamentos
+        if(agendamentosLista.length == 0){
+         //caso o usuario não possua agendamentos
+         
+        }
+        //exibir agendamento do cliente
+        for(let i = 0; i < agendamentosLista.length; i++){
+            //exibir lista com indice de cada elemento da lista
+            let suporte = agendamentosLista[i]//armazenar objeto
+            await ctx.reply(`[ ${i} ] = ${suporte.data_agendamento}`)//exibir objeto em lista
+
+        }
+        await ctx.reply("🌟 Selecione o número do agendamento que deseja cancelar: 📅")
 
     }catch(erros){
         console.error('erro no canclamento de agendamento ' + erros)
